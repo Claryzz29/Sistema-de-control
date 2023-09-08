720b1ce98cf248d68bf21643a20ac5827573f877
Claryzz29 <144208257+Claryzz29@users.noreply.github.com>
2023-09-08 15:13:44 -0600

    Add files via upload
    Sistema de control de registro de estudiantes y docentes

---
+++ App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import Component from './Component';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Escuela Secundaria Jaime Torres Bodet
+        </p>
+         
+      </header>
+    </div>
+  );
+  
+}
+
+export default App;
+
+
